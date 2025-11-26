101deac071f12b3c392b8b5919c69f4328780164
Johaly <johalypolanco13@gmail.com>
2025-11-26 15:46:21 -0500

    Agregando los layouts

---
+++ RealStateApp/Areas/Administration/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealStateApp.Areas.Administration.Controllers
+{
+
+    [Area("Administration")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealStateApp/Areas/Clients/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealStateApp.Areas.Clients.Controllers
+{
+    [Area("Clients")]
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
