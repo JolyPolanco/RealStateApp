04cc8101fafb2cae591f52cf157bca842f00d5c7
kelvinkodad07 <kelvinkodaddiaz@gmail.com>
2025-12-05 18:37:40 -0300

    Agregando funcionalidades de la pantalla principal

---
+++ RealStateApp.Core.Application/ViewModels/GeneralLayout/DataPropertyHomeViewModel.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace RealStateApp.Core.Application.ViewModels.GeneralLayout
+{
+    public class DataPropertyHomeViewModel
+    {
+
+        public int Id { get; set; }
+        public required string Code { get; set; }
+        public required decimal Price { get; set; }
+        public required double SizeInMeters { get; set; }
+        public required int Bedrooms { get; set; }
+        public required int Bathrooms { get; set; }
+        public required string Photo { get; set; }
+        public required string TypeProperty { get; set; }
+        public required string SaleType { get; set; }
+
+        //
+        public string? ClientId { get; set; }
+        public string? AgentId { get; set; }
+
+
+    }
+}
--- RealStateApp/Program.cs
+++ RealStateApp/Program.cs
@@ -59,7 +59,7 @@ app.MapControllerRoute(
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=DataProperty}/{id?}")
     .WithStaticAssets();
 
 app.MapHub<ChatHub>("/chatHub");
