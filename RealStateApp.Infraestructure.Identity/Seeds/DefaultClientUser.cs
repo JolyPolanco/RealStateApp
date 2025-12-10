14474815489a83501d6427449571a6f0672d093b
kelvinkodad07 <kelvinkodaddiaz@gmail.com>
2025-12-10 18:32:47 -0300

    Formalizando usuario cliente del seed

--- RealStateApp.Infraestructure.Identity/Seeds/DefaultClientUser.cs
+++ RealStateApp.Infraestructure.Identity/Seeds/DefaultClientUser.cs
@@ -20,8 +20,9 @@ namespace RealStateApp.Infraestructure.Identity.Seeds
                 FirstName = "Default",
                 LastName = "Client",
                 EmailConfirmed = true, // no requiere confirmación en seed
-                IsActive = true
-                
+                IsActive = true,
+                Photo = "https://i.pinimg.com/originals/33/2b/c7/332bc749fcb06a540e08bca788301e71.jpg"
+
             };
 
             var user = await userManager.FindByEmailAsync(defaultUser.Email);
@@ -30,31 +31,6 @@ namespace RealStateApp.Infraestructure.Identity.Seeds
                 await userManager.CreateAsync(defaultUser, "Client123!");
                 await userManager.AddToRoleAsync(defaultUser, AppRoles.CLIENT.ToString());
             }
-
-
-
-            //otro cliente
-            var Client = new AppUser
-            {
-                UserName = "Nouss",
-                Email = "Nouss@app.com",
-                FirstName = "DefaultClose",
-                LastName = "KIOClient",
-                EmailConfirmed = true, // no requiere confirmación en seed
-                IsActive = true,
-                Photo = "https://i.pinimg.com/originals/33/2b/c7/332bc749fcb06a540e08bca788301e71.jpg"
-
-            };
-
-            var _User = await userManager.FindByEmailAsync(Client.Email);
-            if (_User == null)
-            {
-                await userManager.CreateAsync(Client!, "Client123!");
-                await userManager.AddToRoleAsync(Client, AppRoles.CLIENT.ToString());
-            }
-
-
-
 
         }
     }
